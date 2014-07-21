0b36de054a9c05acb039f03d431f59423d00d387
rishabhjain <rishabhjain2795@gmail.com>
2014-07-22 01:51:53 +0300

    Trying term color

--- imdb.py
+++ imdb.py
@@ -3,14 +3,16 @@
 import urllib2
 import re
 import json
+import sys
+from termcolor import colored, cprint
 
 movie = raw_input("Enter Movie: ")
 value = re.compile(r' ')
 movie_add = value.sub('+', movie) #replace ' ' with '+'
-print movie_add
+cprint (movie_add, 'green')
 
 url = "http://www.imdbapi.com/?t=" + movie_add
 
 conn = urllib2.Request(url)
-result = json.load(urllib2.urlopen(conn))
+result = (json.load(urllib2.urlopen(conn)))
 print json.dumps(result)
